8b4943fa9854fb6a54bf3c8eb279de437d05e0d0
Del Ingvaldson <122827541+delingvaldsonskyboxlabs@users.noreply.github.com>
2023-05-04 10:00:43 -0700

    Moved world generator ownership into the registry context

--- DrifterEngine/src/Systems/Core/ChunkManager.cpp
+++ DrifterEngine/src/Systems/Core/ChunkManager.cpp
@@ -38,8 +38,7 @@ void drft::system::ChunkManager::update(const float dt)
 
 	auto biome = _generator->getBiomeType(cameraPosition);
 	service::DebugInfo::instance().putInfo("Virtual Chunks", std::to_string(_chunks.size()));
-	service::DebugInfo::instance().putInfo("Biome",
-		std::string(gen::Biome2String.at(biome.main).data()) + ", " + gen::Biome2String.at(biome.secondary).data());
+	service::DebugInfo::instance().putInfo("Biome", gen::Biome2String.at(biome).data());
 }
 
 void drft::system::ChunkManager::save(cereal::JSONOutputArchive& oarchive)
