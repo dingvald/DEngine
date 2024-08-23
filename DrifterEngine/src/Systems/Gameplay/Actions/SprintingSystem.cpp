743a08a222a71e161c02d4e5345ba7f2e7602e7c
Delling Ingvaldson <delling_13@hotmail.com>
2024-08-23 09:36:59 -0700

    Added trail effect when sprinting

--- DrifterEngine/src/Systems/Gameplay/Actions/SprintingSystem.cpp
+++ DrifterEngine/src/Systems/Gameplay/Actions/SprintingSystem.cpp
@@ -10,6 +10,8 @@
 #include "Components/SpriteChangeRequestComponent.h"
 
 #include "Utility/SpriteIndexer.h"
+#include "Systems/Helpers/SpawnEffect.h"
+#include "Utility/SpriteOptions.h"
 
 using namespace entt::literals;
 
@@ -33,39 +35,33 @@ void drft::system::SprintingSystem::onFixedUpdate()
 			_registry.remove<SprintingComponent>(entity);
 			continue;
 		}
-		if (!_sprintEffects.contains(entity))
+		if (auto render = _registry.try_get<RenderComponent>(entity))
 		{
-			addSprintEffect(_registry, entity);
+			SpriteOptions effectSprite;
+			createSpriteOptionsFromRenderComponent(effectSprite, *render);
+			effectSprite.layer = 1;
+			effectSprite.color = { 100, 100, 100, 200 };
+
+			spawnEffect(_registry,
+				{
+					.frames = { effectSprite },
+					.position = pos.position,
+					.ttl = 30,
+					.fades = true
+				});
 		}
-		_registry.patch<PositionComponent>(_sprintEffects[entity],
-			[pos](PositionComponent& position)
-			{
-				position.position = pos.position;
-			});
-	}
-
-	animateSprintEffects();
-}
-
-void drft::system::SprintingSystem::shutdown()
-{
-	for (auto&& [sprinter, effect] : _sprintEffects)
-	{
-		_registry.destroy(effect);
 	}
 }
 
 void drft::system::SprintingSystem::onSprintingAdded(entt::registry& registry, entt::entity entity)
 {
 	applySprintBuff(registry, entity);
-	addSprintEffect(registry, entity);
 	registry.emplace_or_replace<SpriteChangeRequestComponent>(entity, "sprinting"_hs);
 }
 
 void drft::system::SprintingSystem::onSprintingRemoved(entt::registry& registry, entt::entity entity)
 {
 	removeSprintBuff(registry, entity);
-	removeSprintEffect(registry, entity);
 	registry.emplace_or_replace<SpriteChangeRequestComponent>(entity, "default"_hs);
 }
 
@@ -91,42 +87,6 @@ void drft::system::SprintingSystem::removeSprintBuff(entt::registry& registry, e
 	{
 		stamina->baseConsumption -= 1.f;
 	}
-}
-
-void drft::system::SprintingSystem::addSprintEffect(entt::registry& registry, entt::entity entity)
-{
-	if (_sprintEffects.contains(entity)) return;
-	auto effect = entt::handle{ registry, registry.create() };
-	RenderComponent renderComponent =
-	{
-		.texture = entt::hashed_string("simple_tileset"),
-		.uvSize = {16, 16},
-		.uvCoords = {1, 5},
-		.layer = 4u,
-		.color = { 50,150,50 }
-	};
-	effect.emplace<RenderComponent>(std::move(renderComponent));
-	effect.emplace<PositionComponent>(sf::Vector2i(0,0));
-	_sprintEffects.emplace(entity, effect.entity());
-}
-
-void drft::system::SprintingSystem::removeSprintEffect(entt::registry& registry, entt::entity entity)
-{
-	registry.destroy(_sprintEffects.at(entity));
-	_sprintEffects.erase(entity);
-}
-
-void drft::system::SprintingSystem::animateSprintEffects() const
-{
-	static int frames = 0;
-	if (frames >= 360) frames = 0;
-	for (auto&& [sprinter, effect] : _sprintEffects)
-	{
-		auto& render = _registry.get<RenderComponent>(effect);
-		float alpha = 255 * ((std::sinf(frames * (PI / 180.f)) + 1.f) / 2.f);
-		render.color.a = static_cast<sf::Uint8>(alpha);
-	}
-	frames+=5;
 }
 
 
--- DrifterEngine/src/Systems/Gameplay/Actions/SprintingSystem.h
+++ DrifterEngine/src/Systems/Gameplay/Actions/SprintingSystem.h
@@ -10,19 +10,12 @@ namespace drft::system
 
 		void init() override;
 		void onFixedUpdate() override;
-		void shutdown() override;
 
 	private:
 		void onSprintingAdded(entt::registry& registry, entt::entity);
 		void onSprintingRemoved(entt::registry& registry, entt::entity);
 		void applySprintBuff(entt::registry& registry, entt::entity entity);
 		void removeSprintBuff(entt::registry& registry, entt::entity entity);
-		void addSprintEffect(entt::registry& registry, entt::entity entity);
-		void removeSprintEffect(entt::registry& registry, entt::entity entity);
-		void animateSprintEffects() const;
-
-	private:
-		std::unordered_map<entt::entity, entt::entity> _sprintEffects;
 	};
 }
 
