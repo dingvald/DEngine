ba7594c41ab12901e4cfab3223868d4acd679436
Delling Ingvaldson <delling_13@hotmail.com>
2024-08-28 14:08:12 -0700

    has line of sight now uses raycast helper on world grid

--- DrifterEngine/src/Systems/Helpers/HasLineOfSight.cpp
+++ DrifterEngine/src/Systems/Helpers/HasLineOfSight.cpp
@@ -9,19 +9,13 @@ bool drft::system::hasLineOfSight(entt::const_handle entity, sf::Vector2i target
 {
 	if (auto posComp = entity.try_get<PositionComponent>())
 	{
-		auto tilesInLOS = spatial::getIntPointsAlongLine(posComp->position, targetPosition);
 		const auto& grid = entity.registry()->ctx().get<const spatial::WorldGrid&>();
-		for (auto&& tile : tilesInLOS)
-		{
-			auto entities = grid.entitiesAt(tile,
-				[&entity](entt::entity e) -> bool
-				{
-					return entity.registry()->any_of<LightBlockingComponent>(e);
-				});
-
-			if (!entities.empty()) return false;
-		}
-		return true;
+		auto filterForLightBlocking = [&entity](entt::entity e) -> bool
+			{
+				return entity.registry()->all_of<LightBlockingComponent>(e);
+			};
+		auto entities = grid.castRay(posComp->position, targetPosition, filterForLightBlocking);
+		return entities.empty();
 	}
 	return false;
 }
