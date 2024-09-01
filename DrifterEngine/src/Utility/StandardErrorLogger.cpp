2ac0500772b32791d5bd8ebd3cb3ea20a42543d7
Delling Ingvaldson <delling_13@hotmail.com>
2024-09-01 12:10:54 -0700

    Added warning logger to print warnings in yellow

--- DrifterEngine/src/Utility/StandardErrorLogger.cpp
+++ DrifterEngine/src/Utility/StandardErrorLogger.cpp
@@ -2,3 +2,8 @@
 #include "StandardErrorLogger.h"
 
 ErrorLogger error_logger;
+WarningLogger warning_logger;
+
+const char* RedText = "\033[31m";
+const char* YellowText = "\033[33m";
+const char* DefaultText = "\033[0m";
