d717f878721eb52e15284bffbb891ba046c06da5
Delling Ingvaldson <delling_13@hotmail.com>
2024-10-15 21:09:45 -0700

    Removed chunk directory and replaced with region directory

--- DrifterEngine/src/Engine/CommonEngineDirectories.h
+++ DrifterEngine/src/Engine/CommonEngineDirectories.h
@@ -16,7 +16,7 @@ static const std::filesystem::path STATIC_DATA_DIRECTORY = WORKING_DIRECTORY / "
 static const std::filesystem::path PLAYER_SAVE_FILE_PATH = SAVE_DIRECTORY / "player_save.json";
 static const std::filesystem::path GAMESTATE_SAVE_FILE_PATH = SAVE_DIRECTORY / "gamestate.json";
 static const std::filesystem::path MAIN_REGISTRY_FILE_PATH = SAVE_DIRECTORY / "registry.json";
-static const std::filesystem::path CHUNK_DIRECTORY = SAVE_DIRECTORY / "chunks";
+static const std::filesystem::path REGION_DIRECTORY = SAVE_DIRECTORY / "regions";
 
 static const std::filesystem::path ENTITIES_DIRECTORY = STATIC_DATA_DIRECTORY / "entities";
 static const std::filesystem::path WORLD_GENERATION_FILE_PATH = STATIC_DATA_DIRECTORY / "world_generation.json";
