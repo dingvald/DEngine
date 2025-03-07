f6c2c37640fa47247182ef07f32747a25589ab0c
Del <del.ingvaldson@gmail.com>
2025-03-06 16:26:27 -0800

    Removed the keybinding states because they provided no value

--- DrifterEngine/src/Keybindings/KeybindingStates.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-namespace KeybindingStates
-{
-	static const std::string Gameplay = "gameplay";
-	static const std::string Menu = "menu";
-	static const std::string General = "general";
-}
