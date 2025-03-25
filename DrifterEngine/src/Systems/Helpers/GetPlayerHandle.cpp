125c550f79050dcd68c4432778390d2149046002
Del <del.ingvaldson@gmail.com>
2025-03-25 08:04:43 -0700

    Added error log to player if handle couldn't be found

--- DrifterEngine/src/Systems/Helpers/GetPlayerHandle.cpp
+++ DrifterEngine/src/Systems/Helpers/GetPlayerHandle.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "GetPlayerHandle.h"
 #include <Components/PlayerInputComponent.h>
+#include <Utility/StandardLogger.h>
 
 entt::handle drft::getPlayerHandle(entt::registry& registry)
 {
@@ -11,6 +12,7 @@ entt::handle drft::getPlayerHandle(entt::registry& registry)
     }
     if (playerView.empty())
     {
+        LOG_ERROR("could not create player handle");
         return entt::handle{ registry, entt::null };
     }
     return entt::handle{registry, *playerView.begin()};
@@ -25,6 +27,7 @@ entt::const_handle drft::getPlayerConstHandle(entt::registry& registry)
     }
     if (playerView.empty())
     {
+        LOG_ERROR("could not create player handle");
         return entt::const_handle{ registry, entt::null };
     }
     return entt::const_handle{ registry, *playerView.begin() };
