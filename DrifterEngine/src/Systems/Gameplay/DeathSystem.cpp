6c01987fc85b4ec471ffbbd4b05f93447031be42
Del <del.ingvaldson@gmail.com>
2025-05-12 07:37:36 -0700

    Changed the drop chances so that materials have a 100% total chance of dropping

--- DrifterEngine/src/Systems/Gameplay/DeathSystem.cpp
+++ DrifterEngine/src/Systems/Gameplay/DeathSystem.cpp
@@ -3,13 +3,11 @@
 #include "Factory/EntityFactory.h"
 
 #include "Components/Components.h"
-#include "Components/DescriptionComponent.h"
 #include "Components/MaterialCompositionComponent.h"
 #include <Components/BaseMaterialComponent.h>
 #include <Components/WeightComponent.h>
 #include "Components/PositionComponent.h"
 #include "Components/PlayerInputComponent.h"
-#include "Components/ItemComponent.h"
 
 #include "Events/RequestStateChange.h"
 #include <States/StateIdentifiers.h>
@@ -23,12 +21,11 @@ void drft::system::DeathSystem::update()
 	auto view = _registry.view<component::action::Die, PositionComponent>();
 	for (auto [entity, pos] : view.each())
 	{
-		const float chance = 80.f;
 		if (auto materialComp = _registry.try_get<MaterialCompositionComponent>(entity))
 		{
 			for (auto&& [matName, percent] : materialComp->materials)
 			{
-				if (!rng::percentChance(chance * percent)) continue;
+				if (!rng::percentChance(percent * 100.f)) continue;
 
 				auto dropped = factory.build(matName, _registry);
 				dropped.emplace_or_replace<PositionComponent>(pos);
