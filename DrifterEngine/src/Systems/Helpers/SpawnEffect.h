f82f7a82b613bebc51ddf0c41c3e3aa4f295f75d
Del <del.ingvaldson@gmail.com>
2025-06-05 17:35:59 -0700

    Added some comments so the fields are more obvious

--- DrifterEngine/src/Systems/Helpers/SpawnEffect.h
+++ DrifterEngine/src/Systems/Helpers/SpawnEffect.h
@@ -14,8 +14,8 @@ namespace drft::system
 		float animationSpeed = 0.f;
 		int ttl = 60; // how many ticks the effect is alive for (60 ticks / second)
 		int fadeRate = 0; // decrease in alpha per tick
-		bool loops = false;
-		bool requiresInFOV = true;
+		bool loops = false; // does this effect loop (if its an animation)
+		bool requiresInFOV = true; // does the effect need to be in the player's fov to be visible?
 	};
 
 	entt::entity spawnEffect(entt::registry& registry, EffectStruct&& effect);
